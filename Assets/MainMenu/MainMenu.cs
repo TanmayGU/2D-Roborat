c888cf0d1840eb4ba6b0b71a40d58543917c418c
Your Name <you@example.com>
2024-02-20 16:22:51 +0100

    TestBuild for Playtesting session
    Enemy attacks work

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("TestingScene");
+        SceneManager.LoadSceneAsync("MainLevel");
     }
 
     public void QuitGame()
